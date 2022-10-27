28ea8577a9688ce114f9f071bff804eb349203ad
Kaspar Pedaja <kaspar.pedaja@voco.ee>
2022-10-27 09:04:32 +0300

    Added extra data from API (humidity, visibility, wind speed, air pressure)

--- classes/UI.js
+++ classes/UI.js
@@ -3,14 +3,26 @@ class UI {
         this.description = document.querySelector('#description')
         this.temp = document.querySelector('#temp')
         this.city = document.querySelector('#location')
+        this.wind = document.querySelector('#wind')
+        this.humidity = document.querySelector('#humidity')
+        this.pressure = document.querySelector('#pressure')
+        this.visibility = document.querySelector('#visibility')
     }
 
     drawWeather(data) {
         let celsius = Math.round(parseFloat(data.main.temp) - 273.15);
         let description = data.weather[0].description;
+        let wind = data.wind.speed;
+        let humidity = data.main.humidity
+        let pressure = data.main.pressure
+        let visibility = data.visibility / 1000
 
         this.description.innerHTML = description;
-        this.temp.innerHTML = celsius + '&deg;';
+        this.temp.innerHTML = "Temperature: " + celsius + '&deg;';
         this.city.innerHTML = data.name;
+        this.wind.innerHTML = "Wind speed: " + wind + "km/h";
+        this.humidity.innerHTML = "Humidity: " + humidity + "%";
+        this.pressure.innerHTML = "Air pressure: " + pressure + "mmHg";
+        this.visibility.innerHTML = "Visibility: " + visibility + " km";
     }
 }
